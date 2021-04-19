145f77d60651dc09200fd1b26287ddd01dd11526
Nate <natedoyon@gmail.com>
2021-04-19 13:53:29 -0600

    added webpage for textbook lookup

---
+++ pages/textbook.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insert Textbook</title>
+</head>
+<body>
+    <form action="#" method="post">
+        <label for="isbnField">ISBN:</label>
+        <input type="text" name="isbnField" id="isbnField"><br>
+        <input type="button" id="button" value="Submit">
+    </form>
+    <script src="../js/callBook.js"></script>
+</body>
+</html>
