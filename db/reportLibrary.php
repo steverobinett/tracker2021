5516148cec92ff544503dcd1258142423c90552a
Joe Daysen <joe.daysen@gmail.com>
2021-04-22 14:39:17 -0600

    started report page ui

--- db/reportLibrary.php
+++ db/reportLibrary.php
@@ -1,7 +1,7 @@
 <?php
 
 function reportByTerm($conn, $termCode) {
-    $query = "SELECT COURSETEXTBOOK.*, COURSE.courseFaculty from COURSETEXTBOOK INNER JOIN COURSE ON COURSE.coursePrefix = COURSETEXTBOOK.ctPrefix AND COURSE.courseNumber = COURSETEXTBOOK.ctNumber AND COURSE.courseSection = COURSETEXTBOOK.ctSection and COURSE.courseTerm = COURSETEXTBOOK.ctTerm WHERE COURSETEXTBOOK.ctTerm = ? ORDER BY ctPrefix, ctNumber, courseFaculty";
+    $query = "SELECT COURSETEXTBOOK.ctISBN, COURSETEXTBOOK.ctPrefix, COURSETEXTBOOK.ctNumber, COURSETEXTBOOK.ctSection, CONCAT(TERM.termSEM, ' ', TERM.termYear), CASE WHEN COURSETEXTBOOK.ctRequired = 0 THEN 'No' WHEN COURSETEXTBOOK.ctRequired = 1 THEN 'Yes' END, CASE WHEN COURSETEXTBOOK.ctUseNew = 0 THEN 'No' WHEN COURSETEXTBOOK.ctUseNew = 1 THEN 'Yes' END, CONCAT(FACULTY.facultyFirst, ' ', FACULTY.facultyLast) from COURSETEXTBOOK INNER JOIN COURSE ON COURSE.coursePrefix = COURSETEXTBOOK.ctPrefix AND COURSE.courseNumber = COURSETEXTBOOK.ctNumber AND COURSE.courseSection = COURSETEXTBOOK.ctSection and COURSE.courseTerm = COURSETEXTBOOK.ctTerm INNER JOIN FACULTY on COURSE.courseFaculty = FACULTY.facultyID INNER JOIN TERM on COURSE.courseTerm = TERM.termID WHERE COURSETEXTBOOK.ctTerm = ? ORDER BY ctPrefix, ctNumber, courseFaculty";
     $stmt = $con->prepare($query);
     $stmt->bind_param('i', $termCode);
     $stmt->execute();
@@ -10,11 +10,21 @@ function reportByTerm($conn, $termCode) {
 }
 
 function reportByCourse($conn, $prefix, $number) {
-    $query = "SELECT COURSETEXTBOOK.*, COURSE.courseFaculty from COURSETEXTBOOK INNER JOIN COURSE ON COURSE.coursePrefix = COURSETEXTBOOK.ctPrefix AND COURSE.courseNumber = COURSETEXTBOOK.ctNumber AND COURSE.courseSection = COURSETEXTBOOK.ctSection and COURSE.courseTerm = COURSETEXTBOOK.ctTerm WHERE COURSETEXTBOOK.ctPrefix = ? AND COURSETEXTBOOK.ctNumber = ? ORDER BY ctTerm, courseFaculty";
+    $query = "SELECT COURSETEXTBOOK.ctISBN, COURSETEXTBOOK.ctPrefix, COURSETEXTBOOK.ctNumber, COURSETEXTBOOK.ctSection, CONCAT(TERM.termSEM, ' ', TERM.termYear), CASE WHEN COURSETEXTBOOK.ctRequired = 0 THEN 'No' WHEN COURSETEXTBOOK.ctRequired = 1 THEN 'Yes' END, CASE WHEN COURSETEXTBOOK.ctUseNew = 0 THEN 'No' WHEN COURSETEXTBOOK.ctUseNew = 1 THEN 'Yes' END, CONCAT(FACULTY.facultyFirst, ' ', FACULTY.facultyLast) from COURSETEXTBOOK INNER JOIN COURSE ON COURSE.coursePrefix = COURSETEXTBOOK.ctPrefix AND COURSE.courseNumber = COURSETEXTBOOK.ctNumber AND COURSE.courseSection = COURSETEXTBOOK.ctSection and COURSE.courseTerm = COURSETEXTBOOK.ctTerm INNER JOIN FACULTY on COURSE.courseFaculty = FACULTY.facultyID INNER JOIN TERM on COURSE.courseTerm = TERM.termID WHERE COURSETEXTBOOK.ctPrefix = ? AND COURSETEXTBOOK.ctNumber = ? ORDER BY ctTerm, courseFaculty";
     $stmt = $con->prepare($query);
-    $stmt->bind_param('i', $termCode);
+    $stmt->bind_param('ss', $prefix, $number);
     $stmt->execute();
     $stmt->store_result();
     return $stmt;
 }
+
+function reportByTextbook($conn, $isbn) {
+    $query = "SELECT COURSETEXTBOOK.ctISBN, COURSETEXTBOOK.ctPrefix, COURSETEXTBOOK.ctNumber, COURSETEXTBOOK.ctSection, CONCAT(TERM.termSEM, ' ', TERM.termYear), CASE WHEN COURSETEXTBOOK.ctRequired = 0 THEN 'No' WHEN COURSETEXTBOOK.ctRequired = 1 THEN 'Yes' END, CASE WHEN COURSETEXTBOOK.ctUseNew = 0 THEN 'No' WHEN COURSETEXTBOOK.ctUseNew = 1 THEN 'Yes' END, CONCAT(FACULTY.facultyFirst, ' ', FACULTY.facultyLast) from COURSETEXTBOOK INNER JOIN COURSE ON COURSE.coursePrefix = COURSETEXTBOOK.ctPrefix AND COURSE.courseNumber = COURSETEXTBOOK.ctNumber AND COURSE.courseSection = COURSETEXTBOOK.ctSection and COURSE.courseTerm = COURSETEXTBOOK.ctTerm INNER JOIN FACULTY on COURSE.courseFaculty = FACULTY.facultyID INNER JOIN TERM on COURSE.courseTerm = TERM.termID WHERE COURSETEXTBOOK.ctISBN = ? ORDER BY ctTerm, ctPrefix, ctNumber, courseFaculty";
+    $stmt = $con->prepare($query);
+    $stmt->bind_param('s', $isbn);
+    $stmt->execute();
+    $stmt->store_result();
+    return $stmt;
+}
+
 ?>
