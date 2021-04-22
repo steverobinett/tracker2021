ae9560c9661955d633b7c182b7145b1490956b7e
Joe Daysen <joe.daysen@gmail.com>
2021-04-22 15:39:40 -0600

    skeleton of one report page

--- pages/termReport.php
+++ pages/termReport.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+include ("../php/functionLibrarySkeleton.php");
+include ("../db/dbConnect.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,11 +18,38 @@
 <body>
     <h1>Have a report by term!</h1>
     <label for="term">Term: </label>
+    <form method="post" action="termReport.php" id="selectTerm">
     <select type="dropdown" id = "term" name="term">
 <?php
+    $dropdownData = SelectAllTerm($conn);
+    $dropdownData->bind_result($termID, $termName);
+    while($dropdownData->fetch()) {
+    echo '<option value="'.$termID.'">'.$termName.'"</option>';
+    }
+
+    echo '</select>';
+    echo '</form>';
+    echo '<button type="submit" form="selectTerm" value="submit">Go</button>';
+
+    $reportData = ReportByTerm($conn, $_POST['selectTerm']);
+    $reportData->bind_result($isbn, $prefix, $num, $sec, $term, $req, $usenew, $fac);
+
+    echo 'div class="tablewrap">';
+        echo '<table id="termreport" class="report">';
+            echo '<tr><th>ISBN</th><th>Course Prefix</th><th>Number</th><th>Section</th><th>Required?</th><th>Use Newer?</th><th>Faculty</th>';
+            while($reportData->fetch()) {
+                echo '<tr>';
+                    echo '<td>'.$isbn.'</td>';
+                    echo '<td>'.$prefix.'</td>';
+                    echo '<td>'.$num.'</td>';
+                    echo '<td>'.$sec.'</td>';
+                    echo '<td>'.$req.'</td>';
+                    echo '<td>'.$usenew.'</td>';
+                    echo '<td>'.$fac.'</td>';
+                echo '</tr>';
+            }
+        echo '</table>';
+    echo '</div>';
 
 ?>
-
-
-
 </body>
