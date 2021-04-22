79d6678862f111d4bdf1833cc20b6dae08887e9f
Joe Daysen <joe.daysen@gmail.com>
2021-04-22 13:40:18 -0600

    reorg files

--- db/reportLibrary.php
+++ db/reportLibrary.php
@@ -9,11 +9,12 @@ function reportByTerm($conn, $termCode) {
     return $stmt;
 }
 
-function reportByCourse($conn, $prefix, $number)
+function reportByCourse($conn, $prefix, $number) {
     $query = "SELECT COURSETEXTBOOK.*, COURSE.courseFaculty from COURSETEXTBOOK INNER JOIN COURSE ON COURSE.coursePrefix = COURSETEXTBOOK.ctPrefix AND COURSE.courseNumber = COURSETEXTBOOK.ctNumber AND COURSE.courseSection = COURSETEXTBOOK.ctSection and COURSE.courseTerm = COURSETEXTBOOK.ctTerm WHERE COURSETEXTBOOK.ctPrefix = ? AND COURSETEXTBOOK.ctNumber = ? ORDER BY ctTerm, courseFaculty";
     $stmt = $con->prepare($query);
     $stmt->bind_param('i', $termCode);
     $stmt->execute();
     $stmt->store_result();
     return $stmt;
+}
 ?>
