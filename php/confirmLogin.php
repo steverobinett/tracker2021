4dde02a6bdbb4136f33866becbe24f21d2027e71
Nate <natedoyon@gmail.com>
2021-04-26 13:49:50 -0600

    checks DB for account and adds session

--- php/confirmLogin.php
+++ php/confirmLogin.php
@@ -1,8 +1,31 @@
 <?php
     session_start();
-?>
+    if(isset($_POST['email']) && isset($_POST['password'])) {
 
-<?php
-    echo '<p>Login Succesful</p>';
-    echo '<a href="../index.php">Home</a>';
+        $userEmail = $_POST['email'];
+        $userPass = $_POST['password'];
+
+        require('../db/functionLibrarySkeleton.php');
+        $status = VerifyUser($userEmail, $userPass);
+
+        switch ($status) {
+            case 0:
+                $_SESSION['userFirst'] = $userFirst;
+                header('Location: http://dev.stevenrobinett.com/index.php');
+            break;
+            case 1:
+                echo "Password incorrect";
+            break;
+            case 2:
+                echo "User not found";
+            break;
+            default:
+                echo "VerifyUser ran into an issue: $status";
+        break;
+        }
+    }
+    else {
+        echo "Error";
+    }
+    echo "<br>Done";
 ?>
