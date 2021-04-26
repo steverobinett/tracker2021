445eccfcdf7286edb72f091aeda97a36f23f6e93
Nate <natedoyon@gmail.com>
2021-04-26 14:30:38 -0600

    removed typo "@" sign

--- db/dbConnect.php
+++ db/dbConnect.php
@@ -1,7 +1,7 @@
 <?php
 
 function getConnection() {
-    @$conn = new mysqli('stevenrobinett.com', 'srobinet_TrackerDev', 'imRL10-20bye', 'srobinet_Tracker2000');
+    $conn = new mysqli('stevenrobinett.com', 'srobinet_TrackerDev', 'imRL10-20bye', 'srobinet_Tracker2000');
         if($conn->connect_errno){
              trigger_error("Connection failed: ".$conn->connect_error);
              exit();
