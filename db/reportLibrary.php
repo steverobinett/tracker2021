e8dd4d1fef14a82eb596e27932a39a4b877ed50a
Joe Daysen <joe.daysen@gmail.com>
2021-04-26 14:34:35 -0600

    enabled reporting by term

--- db/reportLibrary.php
+++ db/reportLibrary.php
@@ -2,7 +2,7 @@
 
 function ReportByTerm($conn, $termCode) {
     $query = "SELECT COURSETEXTBOOK.ctISBN, COURSETEXTBOOK.ctPrefix, COURSETEXTBOOK.ctNumber, COURSETEXTBOOK.ctSection, CONCAT(TERM.termSEM, ' ', TERM.termYear), CASE WHEN COURSETEXTBOOK.ctRequired = 0 THEN 'No' WHEN COURSETEXTBOOK.ctRequired = 1 THEN 'Yes' END, CASE WHEN COURSETEXTBOOK.ctUseNew = 0 THEN 'No' WHEN COURSETEXTBOOK.ctUseNew = 1 THEN 'Yes' END, CONCAT(FACULTY.facultyFirst, ' ', FACULTY.facultyLast) from COURSETEXTBOOK INNER JOIN COURSE ON COURSE.coursePrefix = COURSETEXTBOOK.ctPrefix AND COURSE.courseNumber = COURSETEXTBOOK.ctNumber AND COURSE.courseSection = COURSETEXTBOOK.ctSection and COURSE.courseTerm = COURSETEXTBOOK.ctTerm INNER JOIN FACULTY on COURSE.courseFaculty = FACULTY.facultyID INNER JOIN TERM on COURSE.courseTerm = TERM.termID WHERE COURSETEXTBOOK.ctTerm = ? ORDER BY ctPrefix, ctNumber, courseFaculty";
-    $stmt = $con->prepare($query);
+    $stmt = $conn->prepare($query);
     $stmt->bind_param('i', $termCode);
     $stmt->execute();
     $stmt->store_result();
--- pages/termReport.php
+++ pages/termReport.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
-include ("../php/functionLibrarySkeleton.php");
+include ("../db/functionLibrarySkeleton.php");
 include ("../db/dbConnect.php");
+include ("../db/reportLibrary.php");
 ?>
 
 <!DOCTYPE html>
@@ -12,33 +13,36 @@ include ("../db/dbConnect.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Term Report</title>
 </head>
 <body>
     <h1>Have a report by term!</h1>
-    <label for="term">Term: </label>
+    
     <form method="post" action="termReport.php" id="selectTerm">
+    <label for="term">Term: </label>
     <select type="dropdown" id = "term" name="term">
 <?php
+    $conn = getConnection();
     $dropdownData = SelectAllTerm($conn);
     $dropdownData->bind_result($termID, $termName);
+    echo '<option></option>';
     while($dropdownData->fetch()) {
-    echo '<option value="'.$termID.'">'.$termName.'"</option>';
+    echo '<option value="'.$termID.'">'.$termName.'</option>';
     }
 
     echo '</select>';
     echo '</form>';
-    echo '<button type="submit" form="selectTerm" value="submit">Go</button>';
+    echo '<button type="submit" form="selectTerm" id="button" value="submit">Go</button>';
 
-    $reportData = ReportByTerm($conn, $_POST['selectTerm']);
+    $reportData = ReportByTerm($conn, $_POST['term']);
     $reportData->bind_result($isbn, $prefix, $num, $sec, $term, $req, $usenew, $fac);
 
-    echo 'div class="tablewrap">';
-        echo '<table id="termreport" class="report">';
-            echo '<tr><th>ISBN</th><th>Course Prefix</th><th>Number</th><th>Section</th><th>Required?</th><th>Use Newer?</th><th>Faculty</th>';
+    echo '<div class="tablewrap">';
+        echo '<table id="termreport" class="table is-striped is-narrow">';
+            echo '<tr class="thead"><th>ISBN</th><th>Course Prefix</th><th>Number</th><th>Section</th><th>Required?</th><th>Use Newer?</th><th>Faculty</th>';
             while($reportData->fetch()) {
-                echo '<tr>';
+                echo '<tr class="tbody">';
                     echo '<td>'.$isbn.'</td>';
                     echo '<td>'.$prefix.'</td>';
                     echo '<td>'.$num.'</td>';
@@ -52,4 +56,7 @@ include ("../db/dbConnect.php");
     echo '</div>';
 
 ?>
+<script>
+    document.getElementById('button').addEventListener("click", function(){document.getElementById('termreport').className = 'table is-striped is-narrow'});
+</script>
 </body>
