8924cdaeec1fbee129ce129e42208be83e3f5d98
josiah stoltzfus <josiahds101@gmail.com>
2021-04-26 14:42:49 -0600

    added funcionallity to text to course pages

--- php/addTextCourse.php
+++ php/addTextCourse.php
@@ -8,7 +8,19 @@ $text = $_POST['textbook'];
 $course = $_POST['course'];
 
 $test = explode(",",$course);
+//(ctISBN, ctPrefix,ctNumber,ctSection,ctTerm,ctRequired,ctUseNew)
+$query = "SELECT * FROM COURSETEXTBOOK WHERE `ctISBN` = '$text' AND `ctPrefix` = '$test[0]' AND `ctNumber` = '$test[1]' AND `ctSection` = '$test[2]' AND `ctTerm` = '$test[3]'" ;
 
-InsertIntoCourseTextbook($conn, $text,$test[0],$test[1],$test[2],$test[3]);
+$result = $conn->query($query);
+$result = $result->fetch_array();
 
-header("Location: http://dev.stevenrobinett.com/pages/textbookToCourse.php?Status=success");
+if($result == NULL){
+    InsertIntoCourseTextbook($conn, $text,$test[0],$test[1],$test[2],$test[3]);
+    header("Location: http://dev.stevenrobinett.com/pages/textbookToCourse.php?Status=success");
+}
+else{
+    header("Location: http://dev.stevenrobinett.com/pages/textbookToCourse.php?Status=exists");
+}
+
+
+
