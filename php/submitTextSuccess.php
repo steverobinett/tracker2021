fde9aee322682897a62714da4dd6590a939b3211
ZDJ <zdjewett@gmail.com>
2021-04-27 16:13:30 -0600

    built DB connection

--- php/submitTextSuccess.php
+++ php/submitTextSuccess.php
@@ -1,5 +1,25 @@
-<!-- Testing form output from textbook.php -->
-<?php
-    echo "Submission Success";
-    var_dump($_POST);
+<?php 
+    session_start();
 ?>
+
+<?php
+    require '../db/dbConnect.php';
+    $conn = getConnection();
+
+    $textAuthor = $_POST['authorField'];
+    $textEdition = $_POST['editionField'];
+    $textFormat = $_POST['formatField'];
+    $textISBN = $_POST['isbnField2'];
+    $textPublisher = $_POST['publisherField'];
+    $textTitle = $_POST['titleField'];
+    $insertQuery = 'INSERT INTO TEXTBOOK 
+                    VALUES ("'.$textISBN.'","'.$textTitle.'","'.$textAuthor.'","'.$textEdition.'","'.$textPublisher.'","'.$textFormat.'");';
+    $insertResult = $conn->query($insertQuery);
+    if($insertResult) {
+        $_SESSION['textSubmitSuccess'] = true;
+        $_SESSION['prevISBN'] = $textISBN;
+        // $link = "http://dev.stevenrobinett.com/pages/textbook.php";
+        // echo "<a href='".$link."'>Textbook</a>";
+        // header('Location:http://dev.stevenrobinett.com/pages/textbook.php');
+        echo "<script type='text/javascript'> document.location = '../pages/textbook.php'; </script>";
+    }
