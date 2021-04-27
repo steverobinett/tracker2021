3edfa1c32284a2a64f9c5dfefb3dbf9197789a08
ZDJ <zdjewett@gmail.com>
2021-04-27 16:13:10 -0600

    removed superfluous function, changed header

--- php/confirmReg.php
+++ php/confirmReg.php
@@ -3,29 +3,20 @@
 ?>
 
 <?php
-        $server = 'stevenrobinett.com';
-        $user = 'srobinet_TrackerDev';
-        $password = 'imRL10-20bye';
-        $database = 'srobinet_Tracker2000';
+    require '../db/dbConnect.php';
+    $conn = getConnection();
 
-        $conn = new mysqli($server, $user, $password, $database);
-        if(!$conn){
-             die('Connect failed'.mysqli_connect_error());
-        }
-
-        $userEmail = $_POST['userEmail'];
-        // $userPass = password_hash($_POST['password1'], PASSWORD_DEFAULT);
-        $userPass = sha1($_POST['password1'], false);
-        $userFirst = $_POST['userFirst'];
-        $userLast = $_POST['userLast'];
-        $userRole = $_POST['userRole'];
-        $insertQuery = 'INSERT INTO USER 
-                        VALUES ("'.$userEmail.'","'.$userPass.'","'.$userFirst.'","'.$userLast.'","'.$userRole.'");';
-        $insertResult = $conn->query($insertQuery);
-        if($insertResult) {
-            $_SESSION['userFirst'] = $userFirst;
-            $_SESSION['createSuccess'] = true;
-            echo '<a href="../index.php">Home</a>';
-            echo '<script>alert("Registration Successful!")</script>';
-        }
+    $userEmail = $_POST['userEmail'];
+    $userPass = sha1($_POST['password1'], false);
+    $userFirst = $_POST['userFirst'];
+    $userLast = $_POST['userLast'];
+    $userRole = $_POST['userRole'];
+    $insertQuery = 'INSERT INTO USER 
+                    VALUES ("'.$userEmail.'","'.$userPass.'","'.$userFirst.'","'.$userLast.'","'.$userRole.'");';
+    $insertResult = $conn->query($insertQuery);
+    if($insertResult) {
+        $_SESSION['userFirst'] = $userFirst;
+        $_SESSION['createSuccess'] = true;
+        echo "<script type='text/javascript'> document.location = '../index.php'; </script>";
+    }
 ?>
