08ccc657483a3c7cc399e97b2d9f2ac5ea6f6443
Joe Daysen <joe.daysen@gmail.com>
2021-05-03 19:53:01 -0600

    unfancy function for Unicode

--- php/submitTextSuccess.php
+++ php/submitTextSuccess.php
@@ -3,12 +3,31 @@
     require '../db/dbConnect.php';
     $conn = getConnection();
 
+    function unfancy($fancy) {
+        $fixes = false;
+    
+        if($fixes === false) {
+            $fixes = array (
+                json_decode('"\u201C"') => '"',     // left  double quotation mark
+                json_decode('"\u201D"') => '"',     // right double quotation mark
+                json_decode('"\u2018"') => "'",     // left  single quotation mark
+                json_decode('"\u2019"') => "'",     // right single quotation mark
+                json_decode('"\u2032"') => "'",     // prime (minutes, feet)
+                json_decode('"\u2033"') => '"',     // double prime (seconds, inches)
+                json_decode('"\u2013"') => '-',     // en dash
+                json_decode('"\u2014"') => '--',    // em dash
+            );
+        }
+        $normal = strtr($fancy, $fixes);
+        return $normal;
+    }
+
     $textAuthor = $_POST['authorField'];
     $textEdition = $_POST['editionField'];
     $textFormat = $_POST['formatField'];
     $textISBN = $_POST['isbnField2'];
     $textPublisher = $_POST['publisherField'];
-    $textTitle = $_POST['titleField'];
+    $textTitle = unfancy($_POST['titleField']);
     $insertQuery = 'INSERT INTO TEXTBOOK 
                     VALUES ("'.$textISBN.'","'.$textTitle.'","'.$textAuthor.'","'.$textEdition.'","'.$textPublisher.'","'.$textFormat.'");';
     $insertResult = $conn->query($insertQuery);
